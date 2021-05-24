81edb22173c21ac14f04e607da06dac9a40d8b9c
beaupranisaa <beaupranisaa@gmail.com>
2021-05-24 14:48:08 +0000

    fixed bug

--- 2-pyscript/main_02_ArtifactRemoval_Epoching_psd.py
+++ 2-pyscript/main_02_ArtifactRemoval_Epoching_psd.py
@@ -4,4 +4,4 @@ from config import *
 for f in files:
     i = int(f.split("-")[0])
     for j in range(len(fmax_range)):
-        os.system(f'python3 02_ArtifactRemoval_Epoching_psd/02_ArtifactRemoval_Epoching_psd.py par{i} {f} {fmin_range[j]} {fmax_range[j]} {electrode_zone}')
+        os.system(f'python3 02_ArtifactRemoval_Epoching_psd/02_ArtifactRemoval_Epoching_psd.py par{i} {f} {fmin_range[j]} {fmax_range[j]}')

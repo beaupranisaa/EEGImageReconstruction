7096b96fa0e7aabf888d3fe2bc65652e8dea5411
beaupranisaa <beaupranisaa@gmail.com>
2021-05-25 01:58:30 +0000

    moved chosen_freer_gpu to utils

--- 2-pyscript/03_FeatureExtraction/utils.py
+++ 2-pyscript/03_FeatureExtraction/utils.py
@@ -1,6 +1,13 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+import os
+
+def get_freer_gpu():
+    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
+    gpu = f'cuda:{np.argmax(memory_available)}'
+    return gpu
 
 def get_electrode(X, electrodes):
     XX = []
